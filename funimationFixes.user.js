7cdf3c04ccf06d7c97800c7bfe4e6eb8a383c84c
Mike <michael_eric_moore@hotmail.com>
2020-01-23 14:03:34 -0800

    project setup

--- funimationFixes.user.js
+++ funimationFixes.user.js
@@ -1,11 +1,12 @@
+// @typescript:strictNullChecks
 // ==UserScript==
 // @name          Funimation Fixes
 // @namespace     http://yo1.dog
 // @version       1.0.0
 // @description   Fixes issues with funimation.com
 // @author        Mike "yo1dog" Moore
-// @homepageURL   https://github.com/yo1dog/funimationFixes#readme
-// @icon          https://github.com/yo1dog/funimationFixes/raw/master/icon.ico
+// @homepageURL   https://github.com/yo1dog/funimation-fixes#readme
+// @icon          https://github.com/yo1dog/funimation-fixes/raw/master/icon.ico
 // @match         *://www.funimation.com/*
 // @run-at        document-end
 // ==/UserScript==
@@ -26,14 +27,23 @@ function funimationFixes() {
   console.log('funimationFixes - start');
   
   // get the player iframe
-  const playerIFrame = document.getElementById('player');
+  const playerIFrame = /** @type {HTMLIFrameElement | undefined} */(
+    document.getElementById('player')
+  );
   if (!playerIFrame) {
     return;
   }
+  
   playerIFrame.addEventListener('load', () => {
     console.log('funimationFixes - player iframe loaded');
     
-    execOnPage(playerIFrame.contentWindow, playerFixes)
+    const playerWindow = playerIFrame.contentWindow;
+    if (!playerWindow) {
+      console.error(new Error(`Unable to get window from player iframe.`));
+      return;
+    }
+    
+    execOnPage(playerWindow, playerFixes)
     .catch(err => {
       console.error(err);
     });
@@ -48,38 +58,45 @@ function playerFixes() {
     console.log('funimationFixes - playerFixes - found');
     const prevButton = findPrevButton();
     
-    const show = window.show;
+    const show = /** @type {any} */(window).show;
     if (!show) {
       throw new Error(`Unable to get show from window.`);
     }
-
+    
     const pathname = window.location.pathname;
     const match = /\/player\/(\d+)/.exec(window.location.pathname);
     if (!match) {
       throw new Error(`Unable to get experience ID from pathname: ${pathname}`);
     }
     const experienceId = parseInt(match[1], 10);
-
+    
     const result = findCurrentEpisode(show, experienceId);
     if (!result) {
       throw new Error(`Unable to find current episode for experience ID: ${experienceId}`);
     }
-
+    
     const nextEpisode = getNextEpisode(show, result.seasonIndex, result.episodeIndex);
     const prevEpisode = getPrevEpisode(show, result.seasonIndex, result.episodeIndex);
-
+    
     setButtonAction(nextButton, show, nextEpisode);
     setButtonAction(prevButton, show, prevEpisode);
     console.log('funimationFixes - playerFixes - done');
   })();
-
+  
+  /** @returns {HTMLAnchorElement} */
   function findNextButton() {
-    return document.getElementById('funimation-control-next');
+    return /** @type {HTMLAnchorElement} */(
+      document.getElementById('funimation-control-next')
+    );
   }
+  /** @returns {HTMLAnchorElement} */
   function findPrevButton() {
-    return document.getElementById('funimation-control-prev');
+    return /** @type {HTMLAnchorElement} */(
+      document.getElementById('funimation-control-prev')
+    );
   }
   
+  /** @returns {Promise<HTMLAnchorElement>} */
   async function waitForNextButton() {
     return new Promise(resolve => {
       const observer = new MutationObserver(mutations => {
@@ -93,7 +110,11 @@ function playerFixes() {
       observer.observe(document.documentElement, {childList: true, subtree: true});
     });
   }
-
+  
+  /**
+   * @param {any} show 
+   * @param {number} experienceId 
+   */
   function findCurrentEpisode(show, experienceId) {
     for (let seasonIndex = 0; seasonIndex < show.seasons.length; ++seasonIndex) {
       const season = show.seasons[seasonIndex];
@@ -117,7 +138,12 @@ function playerFixes() {
     }
     return null;
   }
-
+  
+  /**
+   * @param {any} show 
+   * @param {number} currentSeasonIndex 
+   * @param {number} currentEpisodeIndex 
+   */
   function getNextEpisode(show, currentSeasonIndex, currentEpisodeIndex) {
     let nextSeasonIndex  = currentSeasonIndex;
     let nextEpisodeIndex = currentEpisodeIndex + 1;
@@ -133,7 +159,12 @@ function playerFixes() {
 
     return show.seasons[nextSeasonIndex].episodes[nextEpisodeIndex];
   }
-
+  
+  /**
+   * @param {any} show 
+   * @param {number} currentSeasonIndex 
+   * @param {number} currentEpisodeIndex 
+   */
   function getPrevEpisode(show, currentSeasonIndex, currentEpisodeIndex) {
     let prevSeasonIndex  = currentSeasonIndex;
     let prevEpisodeIndex = currentEpisodeIndex - 1;
@@ -149,7 +180,12 @@ function playerFixes() {
 
     return show.seasons[prevSeasonIndex].episodes[prevEpisodeIndex];
   }
-
+  
+  /**
+   * @param {HTMLAnchorElement} button 
+   * @param {any} show 
+   * @param {any} episode 
+   */
   function setButtonAction(button, show, episode) {
     button.removeAttribute('onclick');
     button.target = '_top';
@@ -160,32 +196,47 @@ function playerFixes() {
 
 
 /**
- * @param {Function} fn 
- * @param {*} jsonParam
+ * @param {Window} window 
+ * @param {(param?: any) => void} fn 
+ * @param {any} [jsonParam] 
  */
 async function execOnPage(window, fn, jsonParam) {
   const execId = __execIdSeq++;
+  const eventName = 'exec-on-page-complete';
+  
   const script = window.document.createElement('script');
   script.setAttribute('async', '');
   script.textContent = `
-    (
-      async () => await (${fn})(${JSON.stringify(jsonParam)})
+    const currentScript = document.currentScript;
+    (async () =>
+      await (${fn})(${JSON.stringify(jsonParam)})
     )()
-    .then (result => document.dispatchEvent(new CustomEvent('exec-on-page-complete', {detail: {id: ${JSON.stringify(execId)}, result}})))
-    .catch(error  => document.dispatchEvent(new CustomEvent('exec-on-page-complete', {detail: {id: ${JSON.stringify(execId)}, error }})));
+    .then (result => document.dispatchEvent(new CustomEvent(${JSON.stringify(eventName)}, {detail: {id: ${JSON.stringify(execId)}, result}})))
+    .catch(error  => document.dispatchEvent(new CustomEvent(${JSON.stringify(eventName)}, {detail: {id: ${JSON.stringify(execId)}, error }})))
+    .finally(() => {currentScript.remove();})
   `;
   
   const result = await new Promise((resolve, reject) => {
-    window.document.addEventListener('exec-on-page-complete', event => {
-      if (!event.detail || event.detail.id !== execId) {
+    /** @type {EventListener} */
+    const eventListener = event => {
+      if (
+        !(event instanceof CustomEvent) ||
+        !event.detail ||
+        event.detail.id !== execId
+      ) {
         return;
       }
+      
+      window.document.removeEventListener(eventName, eventListener);
+      
       if (event.detail.error) {
         reject(event.detail.error);
         return;
       }
       resolve(event.detail.result);
-    }, {once: true});
+    };
+    
+    window.document.addEventListener(eventName, eventListener);
     window.document.body.appendChild(script);
   });
   
